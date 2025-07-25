c9b3d48da3c393ad2a6af4758708da1745d8af2b
Neil Skinner <reqpro@requaos.com>
2025-07-25 11:20:22 -0400

    fixup

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use chrono::{Duration, Utc};
-use clap::{command, Arg};
+use clap::{command, Arg, Parser};
 use color_eyre::eyre::ContextCompat;
 use color_eyre::{eyre::Report, eyre::WrapErr, Section};
 use cron_parser::parse;
 use std::env;
 use tracing::{info, instrument};
+
+/// Backup TiKV/SurrealDB S3 Tags
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Matching every n hours
+    #[arg(short, long, default_value_t = String::from("4"))]
+    every_n_hours: String,
+
+    /// Minutes forward from the top of the hour to offset match by
+    #[arg(short, long, default_value_t = String::from("30"))]
+    minutes_offset: String,
+
+    /// Matching window for clock skew and/or job trigger delay
+    #[arg(short, long, default_value_t = String::from("20"))]
+    lag_window_in_minutes: String,
+}
 
 #[instrument]
 fn main() -> Result<(), Report> {
@@ -12,59 +29,23 @@ fn main() -> Result<(), Report> {
     color_eyre::install()?;
 
     info!("Processing CLI flags");
-    let matches = command!() // requires `cargo` feature
-        .propagate_version(true)
-        .about("Backup TiKV/SurrealDB S3 Tags")
-        .arg(
-            Arg::new("nHours")
-                .short('n')
-                .long("every-n-hours")
-                .help("Matching every n hours")
-                .default_value("4"),
-        )
-        .arg(
-            Arg::new("mOffset")
-                .short('m')
-                .long("minutes-offset")
-                .help("Minutes forward from the top of the hour to offset match by")
-                .default_value("30"),
-        )
-        .arg(
-            Arg::new("lagWindow")
-                .short('l')
-                .long("lag-window-in-minutes")
-                .help("Matching window for clock skew and/or job trigger delay")
-                .default_value("20"),
-        )
-        .get_matches();
+    let args = Args::parse();
+    let lag = args
+        .lag_window_in_minutes
+        .parse()
+        .wrap_err("Unable to parse lag-window-in-minutes argument value")?;
+    let checks = periods(&args.minutes_offset, &args.every_n_hours);
 
-    let every_n_hours = if let Some(value) = matches.get_one::<String>("nHours") {
-        value
-    } else {
-        &String::from("4")
-    };
-    let minutes_offset = if let Some(value) = matches.get_one::<String>("mOffset") {
-        value
-    } else {
-        &String::from("30")
-    };
-    let lag_windows_in_minutes = if let Some(value) = matches.get_one::<String>("lagWindow") {
-        value
-    } else {
-        &String::from("20")
-    };
-    // (cron-expr, tag, match-end-of-period)
-    let checks = periods(minutes_offset, every_n_hours);
-
+    info!("Capturing current UTC time and adjusting within lag window");
     let now = Utc::now();
     // Need to subtract a few minutes to catch the current trigger.
     // 1/4 of the lag window feels right.
-    let lag = lag_windows_in_minutes.parse()?;
     let now_comparison_value = now
         .checked_sub_signed(Duration::minutes(lag / 4))
         .wrap_err("Unable to apply jitter to current UTC timestamp")
         .suggestion("Check the system clock")?;
 
+    info!("Processing list of tag checks");
     let mut tags: Vec<String> = Vec::new();
     for check in checks {
         if let Ok(next) = parse(check.0.as_str(), &now_comparison_value) {
@@ -78,12 +59,7 @@ fn main() -> Result<(), Report> {
             if diff.num_seconds().abs() < lag {
                 tags.push(check.1.clone());
             }
-            println!(
-                "- {} when: {} match: {}",
-                check.1,
-                next.to_rfc3339(),
-                diff.num_seconds().abs() < lag
-            );
+            info!(target: "match_attempt_results", tag = check.1, when = next.to_rfc3339(), matched = diff.num_seconds().abs() < lag);
         }
     }
 
@@ -91,11 +67,12 @@ fn main() -> Result<(), Report> {
         "{{\"TagSet\":[{{\"Key\":\"standard\",\"Value\":\"1\"}}{}]}}",
         tags.join("")
     );
-    println!("{}", output);
+    info!(output);
+    print!("{}", output);
     Ok(())
 }
 
-fn periods(minutes_offset: &String, every_n_hours: &String) -> Vec<(String,String,bool)> {
+fn periods(minutes_offset: &String, every_n_hours: &String) -> Vec<(String, String, bool)> {
     // always tag as standard, so manual runs get tagged for lifecycle rules
     // let standard = (
     //     format!("{} 0/{} * * *", minutes_offset, every_n_hours),
